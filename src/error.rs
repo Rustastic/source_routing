4af7ce2b17a8ffa93cfcb5ff13af49b4c9cb4c37
kGB <116108379+gibosio@users.noreply.github.com>
2025-11-08 17:15:30 +0100

    added license snippet

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+	/*   <source_routing - lib for Rustastic client and server>     Copyright (C) <2025>  <Giulio Bosio>      This program is free software: you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation, either version 3 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License     along with this program.  If not, see <https://www.gnu.org/licenses/>.  	*/
+
 use std::collections::HashMap;
 use wg_2024::{
     network::NodeId,
@@ -33,3 +35,4 @@ impl std::fmt::Display for RouterError {
         write!(f, "{self:?}")
     }
 }
+
--- src/router/network_holder/network/mod.rs
+++ src/router/network_holder/network/mod.rs
@@ -1,3 +1,5 @@
+	/*   <source_routing - lib for Rustastic client and server>     Copyright (C) <2025>  <Giulio Bosio>      This program is free software: you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation, either version 3 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License     along with this program.  If not, see <https://www.gnu.org/licenses/>.  	*/
+
 use crate::error::{
     Result,
     RouterError::{IdAlreadyPresent, IdNotFound, ParentsMalformed, RemoveSelfErr, RouteNotFound},
@@ -359,3 +361,4 @@ fn parents_to_path(parents: &HashMap<NodeId, Option<NodeId>>, destination: NodeI
     path.reverse();
     Ok(path)
 }
+
